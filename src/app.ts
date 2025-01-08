fa1dbe863c9a7142549042c3e2e2352193b6b371
kongsh <ksh080838@gmail.com>
2025-01-08 10:33:50 +0900

    [구성] TS 빌드 환경 설정

--- src/app.ts
+++ src/app.ts
@@ -10,16 +10,17 @@
 // 여러 정적 에셋 디렉토리를 사용하려면 express.static 미들웨어 함수를 여러 번 호출합니다.
 //
 // --------------------------------------------------------------------------
-
+import "dotenv/config";
 import express from "express";
+import type { Express, Request, Response, NextFunction } from "express";
 
-const app = express();
+const app: Express = express();
 
 const HOSTNAME = "localhost";
-const PORT = 5000;
+const PORT = Number(process.env.PORT) ?? 4000;
 const MESSAGE = `웹 서버 구동 http://${HOSTNAME}:${PORT}`;
 
-app.get("/", (req, res, nextFunction) => {
+app.get("/", (req: Request, res: Response, nextFunction: NextFunction) => {
   // 서버 -> 클라이언트 응답(response)
   res.send("<h1>Hello Express.js & TS</h1>");
 });
